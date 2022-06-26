3a698e9d381b8c92e9cd9d19cf845d7e10c11f96
Mark Lin <84740316+mk1209567@users.noreply.github.com>
2022-06-26 18:48:53 -0400

    Update trimadapt.sh

--- src/trimadapt.sh
+++ src/trimadapt.sh
@@ -15,4 +15,4 @@ java -jar /gpfs/share/apps/trimmomatic/0.36/trimmomatic-0.36.jar PE -phred33 \
                     ${readPath}/${read}_R1.paired.fastq.gz ${readPath}/${read}_R1.unpaired.fastq.gz \
                     ${readPath}/${read}_R2.paired.fastq.gz ${readPath}/${read}_R2.unpaired.fastq.gz \
                     ILLUMINACLIP:/gpfs/share/apps/trimmomatic/0.36/adapters/NexteraPE-PE.fa:2:30:10 \
-                    LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+                    LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36 > ../log/trim/${read}_trim.log 2>&1
